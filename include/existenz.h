19c8a4169300ba1e1d1dd689bf7b34782bffc354
Thomas Aubert <thomas.aubert@arturia.com>
2022-04-19 14:03:27 +0200

    improve unit tests definition

--- include/existenz.h
+++ include/existenz.h
@@ -58,14 +58,14 @@ public:                                                                         
 };                                                                                  \
 }                                                                                   \
 
-
+/*
 // NOK
 #define FREE_FUNCTION(function_name)                                                \
 namespace exz {                                                                     \
 class free_function_##function_name {                                               \
                                                                                     \
     template<typename ret_t, typename... args_t>                                    \
-    static constexpr auto check(ret_t(*)(args_t...))                                \
+    static constexpr auto check(ret_t(*)(args_t...) = &function_name)               \
     -> typename                                                                     \
         std::is_same<                                                               \
             decltype(function_name(std::declval<args_t>()...)), ret_t               \
@@ -82,7 +82,7 @@ public:                                                                         
     }                                                                               \
 };                                                                                  \
 }                                                                                   \
-
+*/
 
 // OK
 #define MEMBER(member_name)                                                         \
@@ -113,51 +113,62 @@ public:                                                                         
 
 
 
+
+
+
+
 namespace exz_tests {
+
+    MEMBER_FUNCTION(alala);
+
+    STATIC_MEMBER_FUNCTION(hu);
+
+    STATIC_MEMBER_FUNCTION(u);
+
+    MEMBER(mMember);
 
     namespace A{
 
         struct Test1 {
+
             void alala() {}
+
             static bool hu(int) {return true;}
+
             float mMember;
 
             struct H{
+
                 static void u(){}
+
             };
         };
 
         struct Test2 {
+
             struct H{};
+
         };
 
     }
+
+    static_assert(exz::member_function_alala::exists<A::Test1>(), "EXISTENZ");
+    static_assert(!exz::member_function_alala::exists<A::Test2>(), "EXISTENZ");
+
+    static_assert(exz::static_member_function_hu::exists<A::Test1, bool, int>(), "EXISTENZ");
+    static_assert(!exz::static_member_function_hu::exists<A::Test2, bool, int>(), "EXISTENZ");
+
+    static_assert(exz::static_member_function_u::exists<A::Test1::H>(), "EXISTENZ");
+    static_assert(!exz::static_member_function_u::exists<A::Test2::H>(), "EXISTENZ");
+
+    static_assert(exz::member_mMember::exists<A::Test1, float>(), "EXISTENZ");
+    static_assert(!exz::member_mMember::exists<A::Test2, float>(), "EXISTENZ");
+
+
 }
 
 
 
-MEMBER_FUNCTION(alala);
-
-STATIC_MEMBER_FUNCTION(hu);
-
-STATIC_MEMBER_FUNCTION(u);
-
-MEMBER(mMember);
-
-//FREE_FUNCTION(boujou);
-
-
 ///
 
-static_assert(exz::member_function_alala::exists<exz_tests::A::Test1>(), "EXISTENZ");
-static_assert(!exz::member_function_alala::exists<exz_tests::A::Test2>(), "EXISTENZ");
-
-static_assert(exz::static_member_function_hu::exists<exz_tests::A::Test1, bool, int>(), "EXISTENZ");
-static_assert(!exz::static_member_function_hu::exists<exz_tests::A::Test2, bool, int>(), "EXISTENZ");
-
-static_assert(exz::static_member_function_u::exists<exz_tests::A::Test1::H>(), "EXISTENZ");
-static_assert(!exz::static_member_function_u::exists<exz_tests::A::Test2::H>(), "EXISTENZ");
-
-static_assert(exz::member_mMember::exists<exz_tests::A::Test1, float>(), "EXISTENZ");
-static_assert(!exz::member_mMember::exists<exz_tests::A::Test2, float>(), "EXISTENZ");
 
